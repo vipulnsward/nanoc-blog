88328c628622a3adbef82fe1fe68e5ce1f45341a
Vipul A M <vipulnsward@gmail.com>
2013-01-11 10:03:35 +0530

    Remodel

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'http://rubygems.org'
+source 'http://gems.github.com'
+
+gem 'nanoc3'
+gem 'sinatra'
+gem 'adsf'
+gem 'kramdown'
+gem 'redcarpet'
+gem 'typogruby'
+gem 'rubypants'
---
+++ build.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+
+$output_dir = "/home/sward/work/nginx_web/"
+$logs_dir = "/home/sward/.logs/"
+
+configure do
+  set :port, 2246
+  set :bind, '127.0.0.1'
+end
+
+post '/hook' do
+  current_dir = File.dirname(__FILE__)
+  log_file = "#{$logs_dir}/#{Time.now.iso8601}"
+
+  %x(cd #{current_dir} && git pull origin master)
+  %x(cd #{current_dir} && nanoc compile > #{log_file})
+  %x(rm -R #{$output_dir}/* 2>> #{log_file})
+  %x(cd #{current_dir} && mv #{current_dir}/output/* #{$output_dir}/ 2>> #{log_file})
+end
